c419cf5ade6609fcb8cecc8c2dfa8bcf2b23cb85
Jaipreet Singh <jaipreet@amazon.com>
2017-10-26 19:36:50 -0700

    Implement matrix loader
    user_to_ratings[4356, 197]
    4.0
    user_to_ratings[24674, 197]
    3.0
    user_to_ratings[:,1]
    array([ 0.,  0.,  0., ...,  0.,  0.,  0.])
    np.unique(user_to_ratings[:,1])
    array([ 0.,  1.,  2.,  3.,  4.,  5.])
    np.unique(user_to_ratings[:,1], return_counts=True)
    (array([ 0.,  1.,  2.,  3.,  4.,  5.]), array([16734,   280,   861,  3434,  4610,  3059]))
    np.unique(user_to_ratings[:,2], return_counts=True)
    (array([ 0.,  1.,  2.,  3.,  4.,  5.]), array([28920,    20,    13,    15,     7,     3]))
    np.unique(user_to_ratings[:,3], return_counts=True)
    (array([ 0.,  1.,  2.,  3.,  4.,  5.]), array([27312,    93,   142,   455,   590,   386]))

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,48 @@
 import numpy as np
 
-def load_data_as_np():
+
+def load_data_from_txt():
     raw_training_data = np.loadtxt('../data/TrainingRatings.txt', delimiter=',', skiprows=1)
     print(raw_training_data.shape)
+    np.save('../data/modified/raw_data_np.npy', raw_training_data)
+
+
+def load_data_from_saved_py():
+    raw_training_data = np.load('../data/modified/raw_data_np.npy')
+    print(raw_training_data.shape)
+    return raw_training_data
+
+
+userid_to_idx = {}
+movieid_to_idx = {}
+
+
+def build_user_x_movie_matrix(raw_training_data):
+    row, column = raw_training_data.shape
+
+    num_users = len(np.unique(raw_training_data[:,1], return_counts=True)[0])
+    num_movies = len(np.unique(raw_training_data[:,0], return_counts=True)[0])
+    user_to_ratings = np.empty((num_users, num_movies))
+
+    curr_x = 0
+    curr_y = 0
+    for i in range(row):
+        curr_training_sample = raw_training_data[i]
+        movie_id = curr_training_sample[0]
+        user_id = curr_training_sample[1]
+        rating = curr_training_sample[2]
+
+        if user_id not in userid_to_idx:
+            userid_to_idx[user_id] = curr_x
+            curr_x += 1
+
+        if movie_id not in movieid_to_idx:
+            movieid_to_idx[movie_id] = curr_y
+            curr_y += 1
+
+        user_idx = userid_to_idx[user_id]
+        movie_idx = movieid_to_idx[movie_id]
+
+        user_to_ratings[user_idx][movie_idx] = rating
+
+    return user_to_ratings
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import data_loader
 
 if __name__ == "__main__":
-    data_loader.load_data_as_np();
+    raw_training_data= data_loader.load_data_from_saved_py()
+
+    user_to_ratings = data_loader.build_user_x_movie_matrix(raw_training_data)
