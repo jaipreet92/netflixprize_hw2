002ac363f2dc682cd40a17d8ff2adf2675a9bb10
Jaipreet Singh <jaipreet@amazon.com>
2017-11-01 00:00:36 -0700

    kdtree using all movies.

--- src/kdtree.py
+++ src/kdtree.py
@@ -1,8 +1,10 @@
 import data_loader
 from scipy.spatial import KDTree
 import numpy as np
+import math
 
-import main
+import kdtree_main
+import similarity
 
 # Map of user_id to its KDTree
 userid_to_kdtree_map = {}
@@ -18,14 +20,92 @@ def build_kdtree_for_user(user_to_movies_matrix, user_id):
     user_idx = data_loader.userid_to_idx[user_id]
 
     # Find all movies current user has rated.
-    user_rated_movies = user_to_movies_matrix[:, np.where(user_to_movies_matrix[user_idx, :] != 0.0)[0]]
+    user_rated_movies = user_to_movies_matrix[:, np.where(user_to_movies_matrix[user_idx, :] != -10.0)[0]]
 
     # Create a KDTree with the movies the user has rated.
     user_kd_tree = KDTree(user_rated_movies)
     userid_to_kdtree_map[user_id] = user_kd_tree
 
     # Get the indexes of the K nearest neighbours
-    k_nn_idxs = user_kd_tree.query(user_rated_movies[user_idx], k=main.K + 1)[1][1:]
-    userid_to_knnidxs_map[user_id] = k_nn_idxs
+    k_nn_distances, k_nn_idxs = user_kd_tree.query(user_rated_movies[user_idx], k=kdtree_main.K + 1)
+    userid_to_knnidxs_map[user_id] = k_nn_idxs[1:]
 
     return user_kd_tree
+
+
+def get_deviation_from_mean_matrix(user_to_movies_matrix):
+    """
+    Here we are transforming our raw user_to_movies matrix into one where instead of the raw rating value we calculate
+    how the movie's rating deviates from the user's average rating. For example, if User A's average rating is 1.0 and
+    she rates movie as 2.0, and User B's average rating is 4.0 and she rates the movie as 5.0, then they are considered
+    more "similar" rather than just comparing the raw 2.0 and 4.0 ratings. Similarly, this implies that users that have
+    rated movies on opposite sides of their average rating are less similar.
+
+    We assign a value of -10.0 to movies not rated by the user, since the maximum deviation is +/- 5.0. This has the
+    effect that the KD-Tree we build will consider it less similar to the ones that both users have rated.
+    :param user_to_movies_matrix:
+    :return:
+    """
+    num_users = user_to_movies_matrix.shape[0]
+    num_movies = user_to_movies_matrix.shape[1]
+
+    deviation_matrix = np.full((num_users, num_movies), -10.0)
+    for i in range(num_users):
+        user_avg_rating = similarity._get_avg_rating(i, user_to_movies_matrix)
+        for j in range(num_movies):
+            if user_to_movies_matrix[i, j] != 0.0:
+                deviation_matrix[i, j] = user_to_movies_matrix[i, j] - user_avg_rating
+    return deviation_matrix
+
+def predict_using_kdtree(raw_testing_data, user_to_movies_matrix):
+    """
+    Iterates through the test set, and predicts the rating for a user based on the ratings of the "K"
+    nearest or most similar users for the required movie. The RMSE and MAE are aggregated as the predictions
+    are made.
+    :param raw_testing_data:
+    :param user_to_movies_matrix:
+    """
+    not_predicted_count = 0.0
+    predicted_count = 0.0
+
+    mae_sum = 0.0
+    rmse_sum = 0.0
+    num_test_records = raw_testing_data.shape[0]
+    for i in range(num_test_records):
+        movie_id = raw_testing_data[i, 0]
+        user_id = raw_testing_data[i, 1]
+        actual_rating = raw_testing_data[i, 2]
+
+        predicted_rating = _predict_rating_using_kdtree(movie_id, user_id, user_to_movies_matrix)
+        if predicted_rating is None:
+            not_predicted_count += 1
+            print('Could not predict rating for userID {} and movieID {}'.format(user_id, movie_id))
+        else:
+            predicted_count += 1
+            rmse_sum += math.pow(predicted_rating - actual_rating, 2)
+            mae_sum += math.fabs(predicted_rating - actual_rating)
+            print('Actual rating: {} Predicted rating: {}'.format(actual_rating, predicted_rating))
+
+    print('FINAL RESULTS')
+    print('Not predicted values: {}'.format(not_predicted_count))
+    print('Predicted values: {}'.format(predicted_count))
+    print('MAE: {}'.format(mae_sum / predicted_count))
+    print('RMSE: {}'.format(rmse_sum / predicted_count))
+
+
+def _predict_rating_using_kdtree(movie_id, user_id, user_to_movies_deviations, movieid_to_idx=data_loader.movieid_to_idx,
+                                 userid_to_idx=data_loader.userid_to_idx):
+    user_idx = userid_to_idx[user_id]
+    movie_idx = movieid_to_idx[movie_id]
+
+    build_kdtree_for_user(user_to_movies_deviations, user_id)
+    knn_idxs = userid_to_knnidxs_map[user_id]
+
+    neighbour_movie_deviations = user_to_movies_deviations[knn_idxs, movie_idx]
+
+    if len(neighbour_movie_deviations) == 0:
+        return None
+    else:
+        mean_deviation = np.mean(neighbour_movie_deviations[np.where(neighbour_movie_deviations[:]!=-10.0)])
+        avg_rating = similarity._get_avg_rating(user_idx, kdtree_main.user_to_movies_matrix_global)
+        return (avg_rating + mean_deviation)
--- src/kdtree_main.py
+++ src/kdtree_main.py
@@ -1,11 +1,16 @@
 import data_loader
-import predict
+import kdtree
 
-K = 20
+K = 100
+
+user_to_movies_matrix_global = None
 
 if __name__ == "__main__":
     raw_training_data = data_loader.load_data_from_saved_py()
     raw_testing_data = data_loader.load_testing_data_from_saved_npy()
     user_to_movies_matrix = data_loader.build_user_x_movie_matrix(raw_training_data)
+    user_to_movies_matrix_global = user_to_movies_matrix
 
-    predict.predict_using_kdtree(raw_testing_data=raw_testing_data, user_to_movies_matrix=user_to_movies_matrix)
+    deviation_matrix = kdtree.get_deviation_from_mean_matrix(user_to_movies_matrix)
+
+    kdtree.predict_using_kdtree(raw_testing_data=raw_testing_data, user_to_movies_matrix=deviation_matrix)
