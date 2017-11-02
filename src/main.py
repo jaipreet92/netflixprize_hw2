0e9c4a9af7a9c6b6f78307df0f4b9e31e6e5dec0
Jaipreet Singh <jaipreet@amazon.com>
2017-11-01 22:56:56 -0700

    Add precomputed most common movies to main

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     user_to_movies_matrix = data_loader.build_user_x_movie_matrix(raw_training_data)
 
     sampled_movies = np.random.randint(low=0, high=user_to_movies_matrix.shape[1], size=100)
+    #sampled_movies = [1373, 765, 1228, 659, 1498, 902, 649, 1093, 171, 1238, 1378, 129, 975, 705, 455, 478, 629, 271, 145, 875, 1593]
     partial_user_to_movies_matrix = user_to_movies_matrix[:, sampled_movies]
 
     pearson_coefficients = similarity.calculate_pearson_coefficient(partial_user_to_movies_matrix)
